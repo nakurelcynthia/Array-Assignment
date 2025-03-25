7bb2699cdcc9556708e53c6fe03c3dae43b24606
Nakurel <nakurelcynthia@gmail.com>
2025-03-25 22:02:53 +0300

    logged index of last element

---
+++ array.js
@@ -0,0 +1,21 @@
+let arr1 = [3,7,34,90,12];
+console.log({arr1});
+console.log(arr1[arr1.length-1])
+let arr2 = [true, "green", "where",12,56];
+console.log(arr2[arr2.length-1])
+
+let myPets = ["Cow", "Bird", "Snake", "Dog"];
+let animals = myPets.join();
+console.log(animals);
+
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+var arr = arr3.sort();
+console.log(arr);
+
+let arr = ["boy", "man", "girl",  "school", "girl", "woman"];
+
+
+let arr5 = ["the", "way", "x", 4];
+
+
+let word = "renniw";
